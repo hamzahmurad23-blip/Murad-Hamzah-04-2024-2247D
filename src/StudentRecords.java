98a3742f7280ba19f8c02d05e8d669c88bf10c12
Hamzah Murad <hamzahmurad23@gmail.com>
2026-06-22 02:02:01 +0000

    init commit

--- src/StudentRecords.java
+++ src/StudentRecords.java
@@ -5,23 +5,23 @@ public class StudentRecords {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        String fullname, indexnumb, department, level;
+        String fullname, indexnumber, department, level, gender;
         double gpa;
         int age;
 
         System.out.println("Enter Full Name:");
-        input.next();
         fullname = input.nextLine();
         System.out.println("Enter Index Number:");
-        indexnumb = input.next();
+        indexnumber = input.next();
         System.out.println("Enter Department:");
-        input.next();
-        input.next();
-        department = input.next();
+        input.nextLine();
+        department = input.nextLine();
         System.out.println("Enter Level:");
         level = input.next();
         System.out.println("Enter GPA:");
         gpa = input.nextDouble();
+        System.out.println("Gender");
+        gender = input.next();
         System.out.println("Enter Age:");
         age = input.nextInt();
 
@@ -29,8 +29,9 @@ public class StudentRecords {
         //Display Student Profile
         System.out.println("\n===== STUDENT PROFILE =====");
         System.out.println("Full Name: " + fullname);
+        System.out.println("Gender: " + gender);
         System.out.println("Age: " + age);
-        System.out.println("Index Number: " + indexnumb);
+        System.out.println("Index Number: " + indexnumber);
         System.out.println("Department: " + department);
         System.out.println("Level: " + level);
         System.out.println("GPA: " + gpa);
@@ -40,30 +41,30 @@ public class StudentRecords {
 
         if (gpa >= 4.5)
         {
-            System.out.println("First Class");
+            System.out.println("Academic Class: First Class");
         }
         else if (gpa >= 3.5)
         {
-            System.out.println("Second Class Upper");
+            System.out.println("Academic Class: Second Class Upper");
         }
         else if (gpa >= 2.5)
         {
-            System.out.println("Second Class Lower");
+            System.out.println("Academic Class: Second Class Lower");
         }
         else
         {
-            System.out.println("Pass");
+            System.out.println("Academic Class: Pass");
         }
 
         //Determine Adult Status
         int Age;
 
         if (age >= 18)
-            System.out.println("Adult");
+            System.out.println("Adult Status: YES");
         else
-            System.out.println("Minor");
+            System.out.println("Minor Status: NO");
 
-
+        System.out.println("========================");
 
 
 
